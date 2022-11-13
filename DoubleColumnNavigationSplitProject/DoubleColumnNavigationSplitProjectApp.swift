8e69b21ab4d9462a2dced863f1f5d6919de9233f
Karin Prater <gahntpo@yahoo.de>
2022-11-13 10:02:45 +0200

    Initial Commit

---
+++ DoubleColumnNavigationSplitProject/DoubleColumnNavigationSplitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoubleColumnNavigationSplitProjectApp.swift
+//  DoubleColumnNavigationSplitProject
+//
+//  Created by Karin Prater on 13.11.22.
+//
+
+import SwiftUI
+
+@main
+struct DoubleColumnNavigationSplitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
